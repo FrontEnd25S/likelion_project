76921d7f651d53c26411e2733ab89905f1d79d31
Seongho (Steve) Yoo <seonghoyu11@gmail.com>
2025-03-06 23:41:40 -0500

    Delete Internship/liked.js

--- Internship/liked.js
+++
@@ -1,7 +0,0 @@
-let likes = 0;
-let likeBtn = document.getElementById("like");
-
-likeBtn.addEventListener("click", () => {
-  likes = likes === 0 ? 1 : 0;
-  likeBtn.textContent = likes ? `👍 ${likes} Liked`: `👍 Like`;
-});
