aaedc4d08ebcc610b0918bc1ea3838a1c214ce06
Seongho (Steve) Yoo <seonghoyu11@gmail.com>
2025-03-06 23:20:40 -0500

    Update liked.js

--- Internship/liked.js
+++ Internship/liked.js
@@ -1,9 +1,7 @@
 let likes = 0;
 let likeButton = document.getElementById("like");
-let likeCount = document.getElementById("likeCount");
 
 likeButton.addEventListner("click", () => {
   likes = likes === 0 ? 1 : 0;
-  likeCount.textContext = likes;
-  likeButton.textContext = likes ? "👍 Liked" : "👍 Like";
+  likeButton.textContext = likes ? `👍 ${likes} Liked` : "👍 Like";
 });
